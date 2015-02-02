dd18f9f9abd4fe5c7a12354b263741d53f1dda08
Mateusz Wypysiak <mwypysiak@WA-L167.coi.local>
2015-02-02 16:00:53 +0100

    initial commit with almost full markup

---
+++ app/scripts/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name scalacApp
+ * @description
+ * # scalacApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('scalacApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'uiGmapgoogle-maps',
+    'ngGrid'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,67 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name scalacApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the scalacApp
+ */
+angular.module('scalacApp')
+  .config(['uiGmapGoogleMapApiProvider', function(uiGmapGoogleMapApiProvider){
+    uiGmapGoogleMapApiProvider.configure({
+      key: 'AIzaSyB8Q3pMT4KhJ9HFTe-XULhrQoeJXhA6UkM',
+      v: '3.17'
+    });
+  }])
+
+  .filter('nameFilter', function(){
+    return function nameFilter(input){
+      console.log(actual, expected);
+    };
+  })
+  .controller('MainCtrl', ['$location', '$anchorScroll', 'uiGmapGoogleMapApi', function ($location, $anchorScroll, uiGmapGoogleMapApi) {
+
+    var that = this;
+
+    uiGmapGoogleMapApi.then(function(){
+      that.map = {
+        center: {
+          latitude: 45,
+          longitude: -73 },
+        zoom: 8
+      };
+    });
+
+    this.data = [{
+      'id': '1',
+      'name': 'Apple',
+      'url': 'fasfsa',
+      'image':'dasdsa',
+      'long':'12312',
+      'lat': '213214'
+    },{
+      'id': '1',
+      'name': 'Google',
+      'url': 'aaa',
+      'image':'yyy',
+      'long':'867',
+      'lat': '456'
+    }];
+
+    this.gridOptions = {
+      data : 'main.data',
+      columnDefs: [{
+        field: 'name', displayName: 'Name', cellFilter: 'main.nameFilter'
+      }]
+    };
+
+    this.goToSlide = function goToSlide(newHash) {
+      if($location.hash() !== newHash) {
+        $location.hash(newHash);
+      }  else {
+        $anchorScroll();
+      }
+    };
+  }
+]);
